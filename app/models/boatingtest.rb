07e943e3742a5bcdc14fbcdca8b0156c141fe9fb
yoshinator <yoanante@gmail.com>
2018-10-13 23:13:30 -0400

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,27 @@
+# 'BoatingTest' class:
+#
+# should initialize with student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+# BoatingTest.all returns an array of all boating tests
+
+
 class BoatingTest
+  attr_accessor :student, :test_name, :test_status, :instructor
+
+
+  #test1= spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+  @@all = []
+  def initialize(student, test_name, test_status, instructor)
+    @student = student
+    @test_name = test_name
+    @test_status = test_status
+    @instructor = instructor
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,26 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+s1= Student.new("Spongebob")
+s2= Student.new("Patrick")
+
+i1= Instructor.new("Ms.Puff")
+i2= Instructor.new("Mr.Krabs")
+
+t1= s1.add_boating_test("Don't Crash 101", "pending", i1)
+t2= s1.add_boating_test("Safety on the water", "pending", i2)
+t3= s1.add_boating_test("Drinking and boating", "pending", i1)
+t4= s2.add_boating_test("Big boats don't cry", "pending", i1)
+t5= s2.add_boating_test("Advance boater safety", "pending", i2)
+t6= s2.add_boating_test("Don't Crash 101", "pending", i1)
+
+
+Instructor.pass_student(s1,"Don't Crash 101")
+Instructor.pass_student(s1,"Safety on the water")
+Instructor.fail_student(s1,"Drinking and boating")
+
+Instructor.student_grade_percentage("Spongebob")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
