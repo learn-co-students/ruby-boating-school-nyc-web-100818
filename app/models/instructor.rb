d465b20b383775133a1cc6e6a7fee932852380dc
brewchetta <chett.tiller@gmail.com>
2018-10-15 14:55:42 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,33 @@
 class Instructor
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student, test_name)
+    test_name.status = "passed"
+  end
+
+  def fail_student(student, test_name)
+    test_name.status = "failed"
+  end
+
+  def grade_percentage(student)
+    t = 0
+    return "This student has no tests!" if student.boating_tests.length < 1
+    student.boating_tests.each do |bt|
+      t += 1 if bt.status == "passed"
+    end
+    "#{(t / student.boating_tests.length)}% passed"
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,14 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+spongebob = Student.new("Spongebob")
+patrick = Student.new("Patrick")
+
+puff = Instructor.new("Ms.Puff")
+krabs = Instructor.new("Mr.Krabs")
+
+crash101 = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
